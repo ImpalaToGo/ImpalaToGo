ImpalaToGo/ImpalaToGo

commit 7d9b7e22ae416f328889996dfdf059e529c63860

Adding expr-test; also classes RawValue and separating PrimitiveType into its own .h. Adding arithmetic ops tests and fixing type propagation rules and AnalyzerTest.java. Some fixes in order to get select w/o from clause working

be/src/service/plan-executor.cc
@@ -32,15 +32,23 @@ using namespace apache::thrift::transport;
     } \
   } while (false)
 
-#define THROW_IF_EXC(env, exc_class, throwable_to_string_id) \
+#define THROW_IF_EXC(env, exc_class, throwable_to_string_id_) \
   do { \
     jthrowable exc = (env)->ExceptionOccurred(); \
     if (exc != NULL) { \
-     jstring msg = (jstring) env->CallObjectMethod(exc, (throwable_to_string_id)); \
+     jstring msg = (jstring) env->CallObjectMethod(exc, (throwable_to_string_id_)); \
      jboolean is_copy; \
      const char* c_msg = reinterpret_cast<const char*>((env)->GetStringUTFChars(msg, &is_copy)); \
      (env)->ExceptionClear(); \
      (env)->ThrowNew((exc_class), c_msg); \
+     return; \
+    } \
+  } while (false)
+
+#define RETURN_IF_EXC(env) \
+  do { \
+    jthrowable exc = (env)->ExceptionOccurred(); \
+    if (exc != NULL) { \
      return; \
     } \
   } while (false)
@@ -75,19 +83,120 @@ Status PlanExecutor::FetchResult(RowBatch** batch) {
   return Status::OK;
 }
 
-Status DeserializeRequest(
-    JNIEnv* env,
-    jbyteArray execute_plan_request_bytes,
-    ObjectPool* obj_pool,
-    ExecNode** plan,
-    DescriptorTbl** descs,
-    vector<Expr*>* select_list_exprs) {
+class PlanExecutorAdaptor {
+ public:
+  PlanExecutorAdaptor(JNIEnv* env_, jbyteArray thrift_execute_plan_request,
+                      jboolean as_ascii, jobject result_queue)
+    : env_(env_), thrift_execute_plan_request_(thrift_execute_plan_request),
+      as_ascii_(as_ascii), result_queue_(result_queue) {}
 
+  // Indicate error by throwing a new java exception.
+  void Init();
+
+  // Indicate error by throwing a new java exception.
+  void Exec();
+
+  // Indicate error by throwing a new java exception.
+  void AddResultRow(TupleRow* row);
+
+  PlanExecutor* executor() { return executor_.get(); }
+
+  const vector<Expr*>& select_list_exprs() const { return select_list_exprs_; }
+
+  ExecNode* plan() { return plan_; }
+
+  jclass impala_exc_cl() { return impala_exc_cl_; }
+
+ private:
+  JNIEnv* env_;
+  jbyteArray thrift_execute_plan_request_;
+  bool as_ascii_;
+  jobject result_queue_;
+  ObjectPool obj_pool_;
+  ExecNode* plan_;
+  DescriptorTbl* descs_;
+  vector<Expr*> select_list_exprs_;
+  scoped_ptr<PlanExecutor> executor_;
+
+  // classes
+  jclass impala_exc_cl_;
+  jclass throwable_cl_;
+  jclass blocking_queue_if_;
+  jclass result_row_cl_;
+  jclass column_value_cl_;
+
+  // methods
+  jmethodID throwable_to_string_id_;
+  jmethodID put_id_;
+  jmethodID result_row_ctor_, add_to_col_vals_id_;
+  jmethodID column_value_ctor_;
+
+  // fields
+  jfieldID bool_val_field_;
+  jfieldID int_val_field_;
+  jfieldID long_val_field_;
+  jfieldID double_val_field_;
+  jfieldID string_val_field_;
+
+  // Indicate error by throwing a new java exception.
+  void DeserializeRequest();
+};
+
+void PlanExecutorAdaptor::Init() {
+  // setup
+  impala_exc_cl_ = env_->FindClass("com/cloudera/impala/common/ImpalaException");
+  if (impala_exc_cl_ == NULL) {
+    if (env_->ExceptionOccurred()) env_->ExceptionDescribe();
+    return;
+  }
+  jclass throwable_cl_ = env_->FindClass("java/lang/Throwable");
+  if (throwable_cl_ == NULL) {
+    if (env_->ExceptionOccurred()) env_->ExceptionDescribe();
+    return;
+  }
+  jmethodID throwable_to_string_id_ =
+      env_->GetMethodID(throwable_cl_, "toString", "()Ljava/lang/String;");
+  if (throwable_to_string_id_ == NULL) {
+    if (env_->ExceptionOccurred()) env_->ExceptionDescribe();
+    return;
+  }
+
+  blocking_queue_if_ = env_->FindClass("java/util/concurrent/BlockingQueue");
+  THROW_IF_EXC(env_, impala_exc_cl_, throwable_to_string_id_);
+  put_id_ = env_->GetMethodID(blocking_queue_if_, "put", "(Ljava/lang/Object;)V");
+  THROW_IF_EXC(env_, impala_exc_cl_, throwable_to_string_id_);
+
+  result_row_cl_ = env_->FindClass("com/cloudera/impala/thrift/TResultRow");
+  THROW_IF_EXC(env_, impala_exc_cl_, throwable_to_string_id_);
+  result_row_ctor_ = env_->GetMethodID(result_row_cl_, "<init>", "()V");
+  THROW_IF_EXC(env_, impala_exc_cl_, throwable_to_string_id_);
+  add_to_col_vals_id_ =
+      env_->GetMethodID(result_row_cl_, "addToColVals",
+                       "(Lcom/cloudera/impala/thrift/TColumnValue;)V");
+  THROW_IF_EXC(env_, impala_exc_cl_, throwable_to_string_id_);
+
+  column_value_cl_ = env_->FindClass("com/cloudera/impala/thrift/TColumnValue");
+  THROW_IF_EXC(env_, impala_exc_cl_, throwable_to_string_id_);
+  column_value_ctor_ = env_->GetMethodID(column_value_cl_, "<init>", "()V");
+  THROW_IF_EXC(env_, impala_exc_cl_, throwable_to_string_id_);
+  bool_val_field_ = env_->GetFieldID(column_value_cl_, "boolVal", "Z");
+  THROW_IF_EXC(env_, impala_exc_cl_, throwable_to_string_id_);
+  int_val_field_ = env_->GetFieldID(column_value_cl_, "intVal", "I");
+  THROW_IF_EXC(env_, impala_exc_cl_, throwable_to_string_id_);
+  long_val_field_ = env_->GetFieldID(column_value_cl_, "longVal", "J");
+  THROW_IF_EXC(env_, impala_exc_cl_, throwable_to_string_id_);
+  double_val_field_ = env_->GetFieldID(column_value_cl_, "doubleVal", "D");
+  THROW_IF_EXC(env_, impala_exc_cl_, throwable_to_string_id_);
+  string_val_field_ = env_->GetFieldID(column_value_cl_, "stringVal", "Ljava/lang/String;");
+  THROW_IF_EXC(env_, impala_exc_cl_, throwable_to_string_id_);
+}
+
+void PlanExecutorAdaptor::DeserializeRequest() {
   // TODO: Find out why using plan_buf directly does not work.
   // Copy java byte array into native byte array.
   jboolean is_copy = false;
-  int plan_buf_size = env->GetArrayLength(execute_plan_request_bytes);
-  jbyte* plan_buf = env->GetByteArrayElements(execute_plan_request_bytes, &is_copy);
+  int plan_buf_size = env_->GetArrayLength(thrift_execute_plan_request_);
+  jbyte* plan_buf = env_->GetByteArrayElements(thrift_execute_plan_request_, &is_copy);
   uint8_t native_plan_bytes[plan_buf_size];
   for (int i = 0; i < plan_buf_size; i++) {
     native_plan_bytes[i] = plan_buf[i];
@@ -101,17 +210,71 @@ Status DeserializeRequest(
   shared_ptr<TProtocol> tproto = tproto_factory.getProtocol(tmem_transport);
   exec_request.read(tproto.get());
 
-  RETURN_IF_ERROR(ExecNode::CreateTree(obj_pool, exec_request.plan, plan));
-  RETURN_IF_ERROR(DescriptorTbl::Create(obj_pool, exec_request.descTbl, descs));
-  RETURN_IF_ERROR(
-      Expr::CreateExprTrees(obj_pool, exec_request.selectListExprs, select_list_exprs));
+  if (exec_request.__isset.plan) {
+    THROW_IF_ERROR(ExecNode::CreateTree(&obj_pool_, exec_request.plan, &plan_), env_,
+                   impala_exc_cl_);
+  } else {
+    plan_ = NULL;
+  }
+  if (exec_request.__isset.descTbl) {
+    THROW_IF_ERROR(DescriptorTbl::Create(&obj_pool_, exec_request.descTbl, &descs_), env_,
+                   impala_exc_cl_);
+  } else {
+    descs_ = NULL;
+  }
+  // TODO: check that (descs_ == NULL) == (plan_ == NULL)
+  THROW_IF_ERROR(
+      Expr::CreateExprTrees(&obj_pool_, exec_request.selectListExprs,
+                            &select_list_exprs_),
+      env_, impala_exc_cl_);
+}
 
-  return Status::OK;
+void PlanExecutorAdaptor::Exec() {
+  DeserializeRequest();
+  RETURN_IF_EXC(env_);
+  if (plan_ == NULL) return;
+  executor_.reset(new PlanExecutor(plan_, *descs_));
+  THROW_IF_ERROR(executor_->Exec(), env_, impala_exc_cl_);
+}
+
+void PlanExecutorAdaptor::AddResultRow(TupleRow* row) {
+  jobject result_row = env_->NewObject(result_row_cl_, result_row_ctor_);
+  THROW_IF_EXC(env_, impala_exc_cl_, throwable_to_string_id_);
+  for (size_t j = 0; j < select_list_exprs_.size(); ++j) {
+    TColumnValue col_val;
+    select_list_exprs_[j]->GetValue(row, as_ascii_, &col_val);
+    jobject java_col_val = env_->NewObject(column_value_cl_, column_value_ctor_);
+    THROW_IF_EXC(env_, impala_exc_cl_, throwable_to_string_id_);
+    if (col_val.__isset.boolVal) {
+      env_->SetBooleanField(java_col_val, bool_val_field_, col_val.boolVal);
+      THROW_IF_EXC(env_, impala_exc_cl_, throwable_to_string_id_);
+    } else if (col_val.__isset.intVal) {
+      env_->SetIntField(java_col_val, int_val_field_, col_val.intVal);
+      THROW_IF_EXC(env_, impala_exc_cl_, throwable_to_string_id_);
+    } else if (col_val.__isset.longVal) {
+      env_->SetLongField(java_col_val, long_val_field_, col_val.longVal);
+      THROW_IF_EXC(env_, impala_exc_cl_, throwable_to_string_id_);
+    } else if (col_val.__isset.doubleVal) {
+      env_->SetDoubleField(java_col_val, double_val_field_, col_val.doubleVal);
+      THROW_IF_EXC(env_, impala_exc_cl_, throwable_to_string_id_);
+    } else if (col_val.__isset.stringVal) {
+      env_->SetObjectField(java_col_val, string_val_field_,
+                           env_->NewStringUTF(col_val.stringVal.c_str()));
+      THROW_IF_EXC(env_, impala_exc_cl_, throwable_to_string_id_);
+    }
+
+    env_->CallVoidMethod(result_row, add_to_col_vals_id_, java_col_val);
+    THROW_IF_EXC(env_, impala_exc_cl_, throwable_to_string_id_);
+  }
+
+  // place row on result_queue
+  env_->CallVoidMethod(result_queue_, put_id_, result_row);
+  THROW_IF_EXC(env_, impala_exc_cl_, throwable_to_string_id_);
 }
 
 extern "C"
 JNIEXPORT void Java_com_cloudera_impala_service_NativePlanExecutor_Init(
-    JNIEnv* env, jclass caller_class) {
+    JNIEnv* env_, jclass caller_class) {
   google::InitGoogleLogging("impala-backend");
   // install libunwind before activating this on 64-bit systems:
   //google::InstallFailureSignalHandler();
@@ -122,110 +285,36 @@ JNIEXPORT void JNICALL Java_com_cloudera_impala_service_NativePlanExecutor_ExecP
     JNIEnv* env, jclass caller_class, jbyteArray thrift_execute_plan_request,
     jboolean as_ascii, jobject result_queue) {
 
-  ExecNode* plan;
-  DescriptorTbl* descs;
-  vector<Expr*> select_list_exprs;
-  ObjectPool obj_pool;
-  DeserializeRequest(env, thrift_execute_plan_request, &obj_pool, &plan, &descs,
-                     &select_list_exprs);
-
-  // setup
-  jclass impala_exc_cl = env->FindClass("com/cloudera/impala/common/ImpalaException");
-  if (impala_exc_cl == NULL) {
-    if (env->ExceptionOccurred()) env->ExceptionDescribe();
-    return;
-  }
-  jclass throwable_cl = env->FindClass("java/lang/Throwable");
-  if (impala_exc_cl == NULL) {
-    if (env->ExceptionOccurred()) env->ExceptionDescribe();
-    return;
-  }
-  jmethodID throwable_to_string_id = env->GetMethodID(throwable_cl, "toString", "()Ljava/lang/String;");
-  if (throwable_to_string_id == NULL) {
-    if (env->ExceptionOccurred()) env->ExceptionDescribe();
-    return;
-  }
-
-  THROW_IF_EXC(env, impala_exc_cl, throwable_to_string_id);
-  jclass blocking_queue_if = env->FindClass("java/util/concurrent/BlockingQueue");
-  THROW_IF_EXC(env, impala_exc_cl, throwable_to_string_id);
-  jmethodID put_id = env->GetMethodID(blocking_queue_if, "put", "(Ljava/lang/Object;)V");
-  THROW_IF_EXC(env, impala_exc_cl, throwable_to_string_id);
-
-  jclass result_row_cl = env->FindClass("com/cloudera/impala/thrift/TResultRow");
-  THROW_IF_EXC(env, impala_exc_cl, throwable_to_string_id);
-  jmethodID result_row_ctor = env->GetMethodID(result_row_cl, "<init>", "()V");
-  THROW_IF_EXC(env, impala_exc_cl, throwable_to_string_id);
-  jmethodID add_to_col_vals_id =
-      env->GetMethodID(result_row_cl, "addToColVals",
-                       "(Lcom/cloudera/impala/thrift/TColumnValue;)V");
-  THROW_IF_EXC(env, impala_exc_cl, throwable_to_string_id);
-
-  jclass column_value_cl = env->FindClass("com/cloudera/impala/thrift/TColumnValue");
-  THROW_IF_EXC(env, impala_exc_cl, throwable_to_string_id);
-  jmethodID column_value_ctor = env->GetMethodID(column_value_cl, "<init>", "()V");
-  THROW_IF_EXC(env, impala_exc_cl, throwable_to_string_id);
-  jfieldID bool_val_field = env->GetFieldID(column_value_cl, "boolVal", "Z");
-  THROW_IF_EXC(env, impala_exc_cl, throwable_to_string_id);
-  jfieldID int_val_field = env->GetFieldID(column_value_cl, "intVal", "I");
-  THROW_IF_EXC(env, impala_exc_cl, throwable_to_string_id);
-  jfieldID long_val_field = env->GetFieldID(column_value_cl, "longVal", "J");
-  THROW_IF_EXC(env, impala_exc_cl, throwable_to_string_id);
-  jfieldID double_val_field = env->GetFieldID(column_value_cl, "doubleVal", "D");
-  THROW_IF_EXC(env, impala_exc_cl, throwable_to_string_id);
-  jfieldID string_val_field = env->GetFieldID(column_value_cl, "stringVal", "Ljava/lang/String;");
-  THROW_IF_EXC(env, impala_exc_cl, throwable_to_string_id);
-
-  Status status;
-  PlanExecutor executor(plan, *descs);
+  PlanExecutorAdaptor adaptor(env, thrift_execute_plan_request, as_ascii, result_queue);
+  adaptor.Init();
+  RETURN_IF_EXC(env);
+  adaptor.Exec();
+  RETURN_IF_EXC(env);
+  const vector<Expr*>& select_list_exprs = adaptor.select_list_exprs();
+  PlanExecutor* executor = adaptor.executor();
 
   // Prepare select list expressions.
   for (size_t i = 0; i < select_list_exprs.size(); ++i) {
-    select_list_exprs[i]->Prepare(executor.runtime_state());
+    select_list_exprs[i]->Prepare(executor->runtime_state());
   }
 
-  THROW_IF_ERROR(executor.Exec(), env, impala_exc_cl);
+  if (adaptor.plan() == NULL) {
+    // no FROM clause: the select list only contains constant exprs
+    // TODO: check this somewhere
+    adaptor.AddResultRow(NULL);
+    RETURN_IF_EXC(env);
+    return;
+  }
+
   scoped_ptr<RowBatch> batch;
   while (true) {
     RowBatch* batch_ptr;
-    THROW_IF_ERROR(executor.FetchResult(&batch_ptr), env, impala_exc_cl);
+    THROW_IF_ERROR(executor->FetchResult(&batch_ptr), env, adaptor.impala_exc_cl());
     batch.reset(batch_ptr);
-
-    // convert batch of rows to TResultRow
     for (int i = 0; i < batch->num_rows(); ++i) {
       TupleRow* row = batch->GetRow(i);
-      jobject result_row = env->NewObject(result_row_cl, result_row_ctor);
-      THROW_IF_EXC(env, impala_exc_cl, throwable_to_string_id);
-      for (size_t j = 0; j < select_list_exprs.size(); ++j) {
-        TColumnValue col_val;
-        select_list_exprs[j]->GetValue(row, as_ascii, &col_val);
-        jobject java_col_val = env->NewObject(column_value_cl, column_value_ctor);
-        THROW_IF_EXC(env, impala_exc_cl, throwable_to_string_id);
-        if (col_val.__isset.boolVal) {
-          env->SetBooleanField(java_col_val, bool_val_field, col_val.boolVal);
-          THROW_IF_EXC(env, impala_exc_cl, throwable_to_string_id);
-        } else if (col_val.__isset.intVal) {
-          env->SetIntField(java_col_val, int_val_field, col_val.intVal);
-          THROW_IF_EXC(env, impala_exc_cl, throwable_to_string_id);
-        } else if (col_val.__isset.longVal) {
-          env->SetLongField(java_col_val, long_val_field, col_val.longVal);
-          THROW_IF_EXC(env, impala_exc_cl, throwable_to_string_id);
-        } else if (col_val.__isset.doubleVal) {
-          env->SetDoubleField(java_col_val, double_val_field, col_val.doubleVal);
-          THROW_IF_EXC(env, impala_exc_cl, throwable_to_string_id);
-        } else if (col_val.__isset.stringVal) {
-          env->SetObjectField(
-              java_col_val, string_val_field, env->NewStringUTF(col_val.stringVal.c_str()));
-          THROW_IF_EXC(env, impala_exc_cl, throwable_to_string_id);
-        }
-
-        env->CallVoidMethod(result_row, add_to_col_vals_id, java_col_val);
-        THROW_IF_EXC(env, impala_exc_cl, throwable_to_string_id);
-      }
-
-      // place row on result_queue
-      env->CallVoidMethod(result_queue, put_id, result_row);
-      THROW_IF_EXC(env, impala_exc_cl, throwable_to_string_id);
+      adaptor.AddResultRow(row);
+      RETURN_IF_EXC(env);
     }
     if (batch->num_rows() < batch->capacity()) {
       break;

be/CMakeLists.txt
@@ -2,6 +2,9 @@
 
 cmake_minimum_required(VERSION 2.6)
 
+# generate CTest input files
+enable_testing()
+
 # set flags for CMAKE_BUILD_TYPE=Debug;
 # suppress warnings for comparison of signed and unsigned integers
 SET(CMAKE_CXX_FLAGS_DEBUG "-g -Wall -Wno-sign-compare")
@@ -52,6 +55,16 @@ add_library(glogstatic STATIC IMPORTED)
 set_target_properties(glogstatic PROPERTIES IMPORTED_LOCATION ${GLOG_STATIC_LIB})
 
 message(STATUS ${GLOG_INCLUDE_DIR})
+
+# find GTest headers and libs
+find_package(GTest REQUIRED)
+include_directories(${GTEST_INCLUDE_DIR})
+set(LIBS ${LIBS} ${GTEST_LIBRARIES})
+add_library(gtest STATIC IMPORTED)
+set_target_properties(gtest PROPERTIES IMPORTED_LOCATION "${GTEST_LIBRARY}")
+
+message(STATUS ${GTEST_INCLUDE_DIR})
+message(STATUS ${GTEST_LIBRARY})
 
 # resolve "#include "<subdir>/<name>.h"
 include_directories(${CMAKE_SOURCE_DIR}/src)

be/src/exprs/CMakeLists.txt
@@ -31,3 +31,22 @@ add_custom_command(
   OUTPUT functions.cc
   COMMAND "./gen-functions.py"
 )
+
+add_executable(expr-test
+  expr-test.cc
+)
+target_link_libraries(expr-test
+  TestUtil
+  Service
+  Exec
+  Exprs
+  Runtime
+  Common
+  thrift
+  ImpalaThrift
+  MockHdfs
+  gtest
+  ${Boost_LIBRARIES}
+)
+
+add_test(expr-test expr-test)

be/src/runtime/CMakeLists.txt
@@ -10,6 +10,7 @@ set(EXECUTABLE_OUTPUT_PATH "${CMAKE_SOURCE_DIR}/build/runtime")
 add_library(Runtime STATIC
   descriptors.cc
   mem-pool.cc
+  raw-value.cc
   runtime-state.cc
   string-value.cc
 )

thirdparty/gtest-1.6.0/CMakeLists.txt
@@ -1,0 +1,240 @@
+########################################################################
+# CMake build script for Google Test.
+#
+# To run the tests for Google Test itself on Linux, use 'make test' or
+# ctest.  You can select which tests to run using 'ctest -R regex'.
+# For more options, run 'ctest --help'.
+
+# BUILD_SHARED_LIBS is a standard CMake variable, but we declare it here to
+# make it prominent in the GUI.
+option(BUILD_SHARED_LIBS "Build shared libraries (DLLs)." OFF)
+
+# When other libraries are using a shared version of runtime libraries,
+# Google Test also has to use one.
+option(
+  gtest_force_shared_crt
+  "Use shared (DLL) run-time lib even when Google Test is built as static lib."
+  OFF)
+
+option(gtest_build_tests "Build all of gtest's own tests." OFF)
+
+option(gtest_build_samples "Build gtest's sample programs." OFF)
+
+option(gtest_disable_pthreads "Disable uses of pthreads in gtest." OFF)
+
+# Defines pre_project_set_up_hermetic_build() and set_up_hermetic_build().
+include(cmake/hermetic_build.cmake OPTIONAL)
+
+if (COMMAND pre_project_set_up_hermetic_build)
+  pre_project_set_up_hermetic_build()
+endif()
+
+########################################################################
+#
+# Project-wide settings
+
+# Name of the project.
+#
+# CMake files in this project can refer to the root source directory
+# as ${gtest_SOURCE_DIR} and to the root binary directory as
+# ${gtest_BINARY_DIR}.
+# Language "C" is required for find_package(Threads).
+project(gtest CXX C)
+cmake_minimum_required(VERSION 2.6.2)
+
+if (COMMAND set_up_hermetic_build)
+  set_up_hermetic_build()
+endif()
+
+# Define helper functions and macros used by Google Test.
+include(cmake/internal_utils.cmake)
+
+config_compiler_and_linker()  # Defined in internal_utils.cmake.
+
+# Where Google Test's .h files can be found.
+include_directories(
+  ${gtest_SOURCE_DIR}/include
+  ${gtest_SOURCE_DIR})
+
+# Where Google Test's libraries can be found.
+link_directories(${gtest_BINARY_DIR}/src)
+
+########################################################################
+#
+# Defines the gtest & gtest_main libraries.  User tests should link
+# with one of them.
+
+# Google Test libraries.  We build them using more strict warnings than what
+# are used for other targets, to ensure that gtest can be compiled by a user
+# aggressive about warnings.
+cxx_library(gtest "${cxx_strict}" src/gtest-all.cc)
+cxx_library(gtest_main "${cxx_strict}" src/gtest_main.cc)
+target_link_libraries(gtest_main gtest)
+
+########################################################################
+#
+# Samples on how to link user tests with gtest or gtest_main.
+#
+# They are not built by default.  To build them, set the
+# gtest_build_samples option to ON.  You can do it by running ccmake
+# or specifying the -Dbuild_gtest_samples=ON flag when running cmake.
+
+if (gtest_build_samples)
+  cxx_executable(sample1_unittest samples gtest_main samples/sample1.cc)
+  cxx_executable(sample2_unittest samples gtest_main samples/sample2.cc)
+  cxx_executable(sample3_unittest samples gtest_main)
+  cxx_executable(sample4_unittest samples gtest_main samples/sample4.cc)
+  cxx_executable(sample5_unittest samples gtest_main samples/sample1.cc)
+  cxx_executable(sample6_unittest samples gtest_main)
+  cxx_executable(sample7_unittest samples gtest_main)
+  cxx_executable(sample8_unittest samples gtest_main)
+  cxx_executable(sample9_unittest samples gtest)
+  cxx_executable(sample10_unittest samples gtest)
+endif()
+
+########################################################################
+#
+# Google Test's own tests.
+#
+# You can skip this section if you aren't interested in testing
+# Google Test itself.
+#
+# The tests are not built by default.  To build them, set the
+# gtest_build_tests option to ON.  You can do it by running ccmake
+# or specifying the -Dgtest_build_tests=ON flag when running cmake.
+
+if (gtest_build_tests)
+  # This must be set in the root directory for the tests to be run by
+  # 'make test' or ctest.
+  enable_testing()
+
+  ############################################################
+  # C++ tests built with standard compiler flags.
+
+  cxx_test(gtest-death-test_test gtest_main)
+  cxx_test(gtest_environment_test gtest)
+  cxx_test(gtest-filepath_test gtest_main)
+  cxx_test(gtest-linked_ptr_test gtest_main)
+  cxx_test(gtest-listener_test gtest_main)
+  cxx_test(gtest_main_unittest gtest_main)
+  cxx_test(gtest-message_test gtest_main)
+  cxx_test(gtest_no_test_unittest gtest)
+  cxx_test(gtest-options_test gtest_main)
+  cxx_test(gtest-param-test_test gtest
+    test/gtest-param-test2_test.cc)
+  cxx_test(gtest-port_test gtest_main)
+  cxx_test(gtest_pred_impl_unittest gtest_main)
+  cxx_test(gtest-printers_test gtest_main)
+  cxx_test(gtest_prod_test gtest_main
+    test/production.cc)
+  cxx_test(gtest_repeat_test gtest)
+  cxx_test(gtest_sole_header_test gtest_main)
+  cxx_test(gtest_stress_test gtest)
+  cxx_test(gtest-test-part_test gtest_main)
+  cxx_test(gtest_throw_on_failure_ex_test gtest)
+  cxx_test(gtest-typed-test_test gtest_main
+    test/gtest-typed-test2_test.cc)
+  cxx_test(gtest_unittest gtest_main)
+  cxx_test(gtest-unittest-api_test gtest)
+
+  ############################################################
+  # C++ tests built with non-standard compiler flags.
+
+  cxx_library(gtest_no_exception "${cxx_no_exception}"
+    src/gtest-all.cc)
+  cxx_library(gtest_main_no_exception "${cxx_no_exception}"
+    src/gtest-all.cc src/gtest_main.cc)
+  cxx_library(gtest_main_no_rtti "${cxx_no_rtti}"
+    src/gtest-all.cc src/gtest_main.cc)
+
+  cxx_test_with_flags(gtest-death-test_ex_nocatch_test
+    "${cxx_exception} -DGTEST_ENABLE_CATCH_EXCEPTIONS_=0"
+    gtest test/gtest-death-test_ex_test.cc)
+  cxx_test_with_flags(gtest-death-test_ex_catch_test
+    "${cxx_exception} -DGTEST_ENABLE_CATCH_EXCEPTIONS_=1"
+    gtest test/gtest-death-test_ex_test.cc)
+
+  cxx_test_with_flags(gtest_no_rtti_unittest "${cxx_no_rtti}"
+    gtest_main_no_rtti test/gtest_unittest.cc)
+
+  cxx_shared_library(gtest_dll "${cxx_default}"
+    src/gtest-all.cc src/gtest_main.cc)
+
+  cxx_executable_with_flags(gtest_dll_test_ "${cxx_default}"
+    gtest_dll test/gtest_all_test.cc)
+  set_target_properties(gtest_dll_test_
+                        PROPERTIES
+                        COMPILE_DEFINITIONS "GTEST_LINKED_AS_SHARED_LIBRARY=1")
+
+  if (NOT MSVC OR NOT MSVC_VERSION EQUAL 1600)
+    # The C++ Standard specifies tuple_element<int, class>.
+    # Yet MSVC 10's <utility> declares tuple_element<size_t, class>.
+    # That declaration conflicts with our own standard-conforming
+    # tuple implementation.  Therefore using our own tuple with
+    # MSVC 10 doesn't compile.
+    cxx_library(gtest_main_use_own_tuple "${cxx_use_own_tuple}"
+      src/gtest-all.cc src/gtest_main.cc)
+
+    cxx_test_with_flags(gtest-tuple_test "${cxx_use_own_tuple}"
+      gtest_main_use_own_tuple test/gtest-tuple_test.cc)
+
+    cxx_test_with_flags(gtest_use_own_tuple_test "${cxx_use_own_tuple}"
+      gtest_main_use_own_tuple
+      test/gtest-param-test_test.cc test/gtest-param-test2_test.cc)
+  endif()
+
+  ############################################################
+  # Python tests.
+
+  cxx_executable(gtest_break_on_failure_unittest_ test gtest)
+  py_test(gtest_break_on_failure_unittest)
+
+  cxx_executable_with_flags(
+    gtest_catch_exceptions_no_ex_test_
+    "${cxx_no_exception}"
+    gtest_main_no_exception
+    test/gtest_catch_exceptions_test_.cc)
+  cxx_executable_with_flags(
+    gtest_catch_exceptions_ex_test_
+    "${cxx_exception}"
+    gtest_main
+    test/gtest_catch_exceptions_test_.cc)
+  py_test(gtest_catch_exceptions_test)
+
+  cxx_executable(gtest_color_test_ test gtest)
+  py_test(gtest_color_test)
+
+  cxx_executable(gtest_env_var_test_ test gtest)
+  py_test(gtest_env_var_test)
+
+  cxx_executable(gtest_filter_unittest_ test gtest)
+  py_test(gtest_filter_unittest)
+
+  cxx_executable(gtest_help_test_ test gtest_main)
+  py_test(gtest_help_test)
+
+  cxx_executable(gtest_list_tests_unittest_ test gtest)
+  py_test(gtest_list_tests_unittest)
+
+  cxx_executable(gtest_output_test_ test gtest)
+  py_test(gtest_output_test)
+
+  cxx_executable(gtest_shuffle_test_ test gtest)
+  py_test(gtest_shuffle_test)
+
+  cxx_executable(gtest_throw_on_failure_test_ test gtest_no_exception)
+  set_target_properties(gtest_throw_on_failure_test_
+    PROPERTIES
+    COMPILE_FLAGS "${cxx_no_exception}")
+  py_test(gtest_throw_on_failure_test)
+
+  cxx_executable(gtest_uninitialized_test_ test gtest)
+  py_test(gtest_uninitialized_test)
+
+  cxx_executable(gtest_xml_outfile1_test_ test gtest_main)
+  cxx_executable(gtest_xml_outfile2_test_ test gtest_main)
+  py_test(gtest_xml_outfiles_test)
+
+  cxx_executable(gtest_xml_output_unittest_ test gtest)
+  py_test(gtest_xml_output_unittest)
+endif()

commit 955773668d2f9d7de9cd3478020f17e2b9b1a808

fixing build failure introduced by 7d9b7e2

be/generated-sources/gen-cpp/CMakeLists.txt
@@ -15,8 +15,6 @@ add_library(ImpalaThrift
   ImpalaService.cpp
   ImpalaService_constants.cpp
   ImpalaService_types.cpp
-  LocalExecutor_constants.cpp
-  LocalExecutor_types.cpp
   Descriptors_constants.cpp
   Descriptors_types.cpp
   PlanNodes_constants.cpp

be/src/service/plan-executor.cc
@@ -14,7 +14,7 @@
 #include "runtime/descriptors.h"
 #include "runtime/row-batch.h"
 #include "gen-cpp/ImpalaService_types.h"
-#include "gen-cpp/LocalExecutor_types.h"
+#include "gen-cpp/ImpalaPlanService_types.h"
 
 using namespace std;
 using namespace boost;