79bb70ef154372214e916500e311fce6ea1154e9
Henry Robinson <henry@cloudera.com>
2012-10-29 22:46:38 -0700

    Add ASL license to all files

--- bin/gen_build_version.py
+++ bin/gen_build_version.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python
-# Copyright (c) 2012 Cloudera, Inc. All rights reserved.
+# Copyright 2012 Cloudera Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 
 # This scrips generates be/src/common/version.cc which contains the build version based
 # on the git hash.
@@ -34,7 +47,20 @@ with open(VERSION_FILE_NAME) as version_file:
 print '\n'.join([version, git_hash, build_time])
 
 preamble = '\
-// Copyright (c) 2012 Cloudera, Inc. All rights reserved.\n\
+// Copyright 2012 Cloudera Inc.\n\
+//\n\
+// Licensed under the Apache License, Version 2.0 (the "License");\n\
+// you may not use this file except in compliance with the License.\n\
+// You may obtain a copy of the License at\n\
+//\n\
+// http://www.apache.org/licenses/LICENSE-2.0\n\
+//\n\
+// Unless required by applicable law or agreed to in writing, software\n\
+// distributed under the License is distributed on an "AS IS" BASIS,\n\
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n\
+// See the License for the specific language governing permissions and\n\
+// limitations under the License.\n\
+\n\
 // This is a generated file, DO NOT EDIT IT.\n\
 // To change this file, see impala/bin/gen_build_version.py\n\
 \n\
