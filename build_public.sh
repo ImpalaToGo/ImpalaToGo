831d90c6782cf1d6f116ac1cb40d7b148f35233e
Henry Robinson <henry@cloudera.com>
2012-11-05 16:14:09 -0800

    Public repo build scripts

---
+++ build_public.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+# Copyright 2012 Cloudera Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Builds the Cloudera Impala frontend and backend. 
+# You must have run thirdparty/download_thirdparty.sh before this script
+
+. bin/impala-config.sh
+
+# Exit on non-true return value
+set -e
+# Exit on reference to uninitialized variable
+set -u
+
+BUILD_THIRDPARTY=0
+TARGET_BUILD_TYPE=Debug
+
+for ARG in $*
+do
+  case "$ARG" in
+    -build_thirdparty)
+      BUILD_THIRDPARTY=1
+    ;;
+  esac
+done
+
+if [ ! -f ${IMPALA_HOME}/thirdparty/gflags-${IMPALA_GFLAGS_VERSION}/.libs/libgflags.so.2 ]
+then
+  BUILD_THIRDPARTY=1
+fi
+
+if [ ${BUILD_THIRDPARTY} -eq 1 ]
+then
+  echo "********************************"
+  echo " Building third-party libraries "
+  echo "********************************"
+  ${IMPALA_HOME}/bin/build_thirdparty.sh -noclean
+fi
+
+echo "******************************"
+echo " Building Impala backend "
+echo "******************************"
+# build common and backend
+cd $IMPALA_HOME
+${IMPALA_HOME}/bin/gen_build_version.py
+cmake -DCMAKE_BUILD_TYPE=$TARGET_BUILD_TYPE .
+cd $IMPALA_HOME/common/function-registry
+make
+cd $IMPALA_HOME/common/thrift
+make
+cd $IMPALA_BE_DIR
+make -j4
+
+# build frontend
+echo "******************************"
+echo " Building Impala frontend "
+echo "******************************"
+cd $IMPALA_FE_DIR
+mvn package -DskipTests=true
---
+++ thirdparty/download_thirdparty.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+# Copyright 2012 Cloudera Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+if [ "x${IMPALA_HOME}" -eq "x"]; then
+  echo "IMPALA_HOME must be set"
+  exit 1;
+fi
+
+cd ${IMPALA_HOME}/thirdparty
+
+echo "Removing everything in ${IMPALA_HOME}/thirdparty"
+git clean -xdf
+
+echo "Fetching gtest"
+wget http://googletest.googlecode.com/files/gtest-${IMPALA_GTEST_VERSION}.zip
+unzip gtest-${IMPALA_GTEST_VERSION}.zip
+rm gtest-${IMPALA_GTEST_VERSION}.zip
+
+echo "Fetching glog"
+wget http://google-glog.googlecode.com/files/glog-${IMPALA_GLOG_VERSION}.tar.gz
+tar xzf glog-${IMPALA_GLOG_VERSION}.tar.gz
+rm glog-${IMPALA_GLOG_VERSION}.tar.gz
+
+echo "Fetching gflags"
+wget http://gflags.googlecode.com/files/gflags-${IMPALA_GFLAGS_VERSION}.zip
+unzip gflags-${IMPALA_GFLAGS_VERSION}.zip
+rm gflags-${IMPALA_GFLAGS_VERSION}.zip
+
+echo "Fetching gperftools"
+wget http://gperftools.googlecode.com/files/gperftools-${IMPALA_GPERFTOOLS_VERSION}.tar.gz
+tar xzf gperftools-${IMPALA_GPERFTOOLS_VERSION}.tar.gz
+rm gperftools-${IMPALA_GPERFTOOLS_VERSION}.tar.gz
+
+echo "Fetching snappy"
+wget http://snappy.googlecode.com/files/snappy-${IMPALA_SNAPPY_VERSION}.tar.gz
+tar xzf snappy-${IMPALA_SNAPPY_VERSION}.tar.gz
+rm snappy-${IMPALA_SNAPPY_VERSION}.tar.gz
+
+echo "Fetching cyrus-sasl"
+wget ftp://ftp.andrew.cmu.edu/pub/cyrus-mail/cyrus-sasl-${IMPALA_CYRUS_SASL_VERSION}.tar.gz
+tar xzf cyrus-sasl-${IMPALA_CYRUS_SASL_VERSION}.tar.gz
+rm cyrus-sasl-${IMPALA_CYRUS_SASL_VERSION}.tar.gz
+
+echo "Fetching mongoose"
+wget http://mongoose.googlecode.com/files/mongoose-${IMPALA_MONGOOSE_VERSION}.tgz
+tar xzf mongoose-${IMPALA_MONGOOSE_VERSION}.tgz
+rm mongoose-${IMPALA_MONGOOSE_VERSION}.tgz
+
+echo "Fetching Apache Hadoop"
+wget http://archive.cloudera.com/cdh4/cdh/4/hadoop-${IMPALA_HADOOP_VERSION}.tar.gz
+tar xzf hadoop-${IMPALA_HADOOP_VERSION}.tar.gz
+rm hadoop-${IMPALA_HADOOP_VERSION}.tar.gz
+
+echo "Fetching Apache Hive"
+wget http://archive.cloudera.com/cdh4/cdh/4/hive-${IMPALA_HIVE_VERSION}.tar.gz
+tar xzf hive-${IMPALA_HIVE_VERSION}.tar.gz
+rm hive-${IMPALA_HIVE_VERSION}.tar.gz
+
+echo "Fetching Apache Thrift"
+wget http://archive.apache.org/dist/thrift/${IMPALA_THRIFT_VERSION}/thrift-${IMPALA_THRIFT_VERSION}.tar.gz
+tar xzf thrift-${IMPALA_THRIFT_VERSION}.tar.gz
+mkdir python-thrift-${IMPALA_THRIFT_VERSION}
+mv thrift-${IMPALA_THRIFT_VERSION}/lib/py/src python-thrift-${IMPALA_THRIFT_VERSION}/thrift
+rm thrift-${IMPALA_THRIFT_VERSION}.tar.gz
+rm -rf thrift-${IMPALA_THRIFT_VERSION}
