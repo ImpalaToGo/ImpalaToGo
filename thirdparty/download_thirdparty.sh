2f2cfb648266d297c8e2001a8db9da6047f0bacc
Lenni Kuff <lskuff@cloudera.com>
2013-07-25 08:36:15 -0700

    Update download_thirdparty.sh to only download required dependencies

--- thirdparty/download_thirdparty.sh
+++ thirdparty/download_thirdparty.sh
@@ -28,42 +28,7 @@ wget http://googletest.googlecode.com/files/gtest-${IMPALA_GTEST_VERSION}.zip
 unzip gtest-${IMPALA_GTEST_VERSION}.zip
 rm gtest-${IMPALA_GTEST_VERSION}.zip
 
-echo "Fetching glog"
-wget http://google-glog.googlecode.com/files/glog-${IMPALA_GLOG_VERSION}.tar.gz
-tar xzf glog-${IMPALA_GLOG_VERSION}.tar.gz
-rm glog-${IMPALA_GLOG_VERSION}.tar.gz
-
-echo "Fetching gflags"
-wget http://gflags.googlecode.com/files/gflags-${IMPALA_GFLAGS_VERSION}.zip
-unzip gflags-${IMPALA_GFLAGS_VERSION}.zip
-rm gflags-${IMPALA_GFLAGS_VERSION}.zip
-
-echo "Fetching gperftools"
-wget http://gperftools.googlecode.com/files/gperftools-${IMPALA_GPERFTOOLS_VERSION}.tar.gz
-tar xzf gperftools-${IMPALA_GPERFTOOLS_VERSION}.tar.gz
-rm gperftools-${IMPALA_GPERFTOOLS_VERSION}.tar.gz
-
 echo "Fetching snappy"
 wget http://snappy.googlecode.com/files/snappy-${IMPALA_SNAPPY_VERSION}.tar.gz
 tar xzf snappy-${IMPALA_SNAPPY_VERSION}.tar.gz
 rm snappy-${IMPALA_SNAPPY_VERSION}.tar.gz
-
-echo "Fetching cyrus-sasl"
-wget ftp://ftp.andrew.cmu.edu/pub/cyrus-mail/cyrus-sasl-${IMPALA_CYRUS_SASL_VERSION}.tar.gz
-tar xzf cyrus-sasl-${IMPALA_CYRUS_SASL_VERSION}.tar.gz
-rm cyrus-sasl-${IMPALA_CYRUS_SASL_VERSION}.tar.gz
-
-echo "Fetching mongoose"
-wget http://mongoose.googlecode.com/files/mongoose-${IMPALA_MONGOOSE_VERSION}.tgz
-tar xzf mongoose-${IMPALA_MONGOOSE_VERSION}.tgz
-rm mongoose-${IMPALA_MONGOOSE_VERSION}.tgz
-
-echo "Fetching Apache Hive"
-wget http://archive.cloudera.com/cdh4/cdh/4/hive-${IMPALA_HIVE_VERSION}.tar.gz
-tar xzf hive-${IMPALA_HIVE_VERSION}.tar.gz
-rm hive-${IMPALA_HIVE_VERSION}.tar.gz
-
-echo "Fetching Apache Thrift"
-wget http://archive.apache.org/dist/thrift/${IMPALA_THRIFT_VERSION}/thrift-${IMPALA_THRIFT_VERSION}.tar.gz
-tar xzf thrift-${IMPALA_THRIFT_VERSION}.tar.gz
-rm thrift-${IMPALA_THRIFT_VERSION}.tar.gz
