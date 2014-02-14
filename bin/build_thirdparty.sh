d503225bf46ae5fb87ff0e78bf25531fa23911c5
Matthew Jacobs <mj@cloudera.com>
2014-02-14 12:45:21 -0800

    [CDH4] Add third-party jar containing Yarn classes used by Impala
    This is necessary because Impala has an admission controller that is configured
    using the same configuration as Llama and Yarn (i.e. a fair-scheduler.xml and
    llama-site.xml). Some Yarn classes (available in CDH5 only) are used to provide
    user to pool resolution, authorization, and accessing pool configurations.
    Impala needs these classes on the classpath in order to support CDH4, so they're
    built as a separate third-party jar. For CDH5, this jar is not necessary and
    will not be included as a dependency. Because this exists in CDH4 only, this
    lives in third-party along with other CDH version-specific dependencies.
    
    Change-Id: I85fc19a86f26df659400434a9c4f186d0029545c
    Reviewed-on: http://gerrit.ent.cloudera.com:8080/1565
    Reviewed-by: Matthew Jacobs <mj@cloudera.com>
    Tested-by: Matthew Jacobs <mj@cloudera.com>
    Tested-by: jenkins

--- bin/build_thirdparty.sh
+++ bin/build_thirdparty.sh
@@ -39,6 +39,7 @@ BUILD_SASL=0
 BUILD_LDAP=0
 BUILD_SNAPPY=0
 BUILD_PPROF=0
+BUILD_CDH4EXTRAS=0
 
 for ARG in $*
 do
@@ -86,9 +87,13 @@ do
       BUILD_ALL=0
       BUILD_PPROF=1
       ;;
+    -cdh4extras)
+      BUILD_ALL=0
+      BUILD_CDH4EXTRAS=1
+      ;;
     -*)
       echo "Usage: build_thirdparty.sh [-noclean] \
-[-avro -glog -thrift -gflags -gtest -re2 -sasl -ldap -snappy -pprof]"
+[-avro -glog -thrift -gflags -gtest -re2 -sasl -ldap -snappy -pprof -cdh4extras]"
       exit 1
   esac
 done
@@ -222,4 +227,10 @@ if [ $BUILD_ALL -eq 1 ] || [ $BUILD_AVRO -eq 1 ]; then
   build_preamble $IMPALA_HOME/thirdparty/avro-c-${IMPALA_AVRO_VERSION} Avro
   cmake .
   make -j4
+fi
+
+# Build cdh4-extras
+if [ $BUILD_ALL -eq 1 ] || [ $BUILD_CDH4EXTRAS -eq 1 ]; then
+  build_preamble $IMPALA_HOME/thirdparty/cdh4-extras cdh4-extras
+  mvn package -DskipTests
 fi
