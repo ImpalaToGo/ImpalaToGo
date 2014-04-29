eb19e38a0682e59d94a88623a37cc1e60aa40445
Matthew Jacobs <mj@cloudera.com>
2014-04-29 12:36:59 -0700

    [CDH4] Disable admission control to work around HUE-994
    On CDH4, Hue does not close queries (HUE-994), so queries
    submitted through Hue will never be released in the admission controller
    and eventually all queries will be rejected. This disables admission
    control on CDH4 to work around this problem.
    
    Change-Id: I1bb1707b1d2c4f8c041b6521e715aa9f7fabc182
    Reviewed-on: http://gerrit.ent.cloudera.com:8080/2399
    Tested-by: jenkins
    Reviewed-by: Matthew Jacobs <mj@cloudera.com>
    Tested-by: Matthew Jacobs <mj@cloudera.com>

--- be/src/statestore/simple-scheduler.cc
+++ be/src/statestore/simple-scheduler.cc
@@ -54,7 +54,9 @@ DECLARE_bool(enable_rm);
 DECLARE_int32(rm_default_cpu_vcores);
 DECLARE_string(rm_default_memory);
 
-DEFINE_bool(disable_admission_control, false, "Disables admission control.");
+// Admission control is disabled by default (CDH4 only) due to HUE-994, Hue doesn't
+// close queries (fixed in CDH5). Default is false on CDH5.
+DEFINE_bool(disable_admission_control, true, "Disables admission control.");
 DEFINE_bool(require_username, false, "Requires that a user be provided in order to "
     "schedule requests. If enabled and a user is not provided, requests will be "
     "rejected, otherwise requests without a username will be submitted with the "
