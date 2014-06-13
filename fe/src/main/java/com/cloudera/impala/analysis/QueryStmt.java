cecb68c1b1a98a63c5c62e0d48131806f6a5b35a
Srinath Shankar <sshankar@cloudera.com>
2014-06-13 15:09:39 -0700

    Ignore order-by without limit in INSERT and CTAS
    Order-by without limit in the query statement corresponding an INSERT
    or CTAS must be ignored because
    i) There is no guarantee on row ordering when the target table is scanned again
       i.e. 'select * from table' may return rows in any order, regardless of how the
       rows were inserted, and
    ii) Ignoring (and not flagging an error) is consistent with the treatment of
       order-by w/o limit in nested queries, union operands etc.
    Currently, an order-by w/o limit in a QueryStmt is only evaluated if the analyzer is
    the root analyzer (has no ancestors).
    However, a new child analyzer is not created for the QueryStmt in an InsertStmt, so this
    technique fails for inserts. The correct thing to do is to use a child analyzer for that
    QueryStmt, but this has spill-over scoping effects for analysis of with clauses.
    
    This patch adds a flag, similar to the isExplain flag to the analyzer to identify
    insert statements.
    
    Change-Id: I9ded587cfea75eca0b7a43ee9b0df0a6c8ecb602
    Reviewed-on: http://gerrit.ent.cloudera.com:8080/3044
    Reviewed-by: Srinath Shankar <sshankar@cloudera.com>
    Tested-by: jenkins
    Reviewed-on: http://gerrit.ent.cloudera.com:8080/3060

--- fe/src/main/java/com/cloudera/impala/analysis/QueryStmt.java
+++ fe/src/main/java/com/cloudera/impala/analysis/QueryStmt.java
@@ -146,8 +146,10 @@ public abstract class QueryStmt extends StatementBase {
     }
 
     sortInfo_ = new SortInfo(orderingExprs, isAscOrder, nullsFirstParams);
-    // order by w/o limit and offset in inline views and union operands are ignored.
-    if (!analyzer.isRootAnalyzer() && !hasLimit() && !hasOffset()) {
+    // order by w/o limit and offset in inline views, union operands and insert statements
+    // are ignored.
+    if (!hasLimit() && !hasOffset() &&
+        (!analyzer.isRootAnalyzer() || analyzer.isInsertStmt())) {
       evaluateOrderBy_ = false;
     } else {
       evaluateOrderBy_ = true;
