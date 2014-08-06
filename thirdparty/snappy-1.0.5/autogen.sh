f7c141812641f17ca1c5c9f03c054ce7683083eb
Sean Mackrory <sean@cloudera.com>
2014-08-06 13:23:49 -0600

    CDH-18430. Snappy build fails for various reasons on various distros
        Using the build scripts shipped with the official Snappy release, the build
        can occasionally fail due to special characters in the path and race
        conditions, and these failures do not always occur consistently. This
        forces autotools to regenerate the build scripts each time and overwrite
        existing scripts, and works around several problems encountered when
        regenerating on older distros including doc_dir not having a default value
        and initializing libtool a different way.
    
    Change-Id: I326a656d4c3c258e5eb71f69f54eee6b6d434290
    (cherry picked from commit acf1fe90264cfda4d72e836e063c32403e35f1e1)
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/4066
    Reviewed-by: Sean Mackrory <sean@cloudera.com>
    Tested-by: Sean Mackrory <sean@cloudera.com>

--- thirdparty/snappy-1.0.5/autogen.sh
+++ thirdparty/snappy-1.0.5/autogen.sh
@@ -1,7 +1,8 @@
 #! /bin/sh -e
 rm -rf autom4te.cache
-aclocal -I m4
-autoheader
-libtoolize --copy
+libtoolize --copy --force
+aclocal -I m4 --force
+autoheader --force
 automake --add-missing --copy
-autoconf
+autoconf --force
+
