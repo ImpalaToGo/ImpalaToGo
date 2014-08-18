9db351275e2e07631f0fd1e15d430230ee945689
Skye Wanderman-Milne <skye@cloudera.com>
2014-08-18 14:32:19 -0700

    IMPALA-1149: read bytes fields as strings in HdfsAvroScanner::MaterializeTuple()
    Hive converts "bytes"-type fields to an array<tinyint> column, which
    we can't even load the metadata for. However, if a bytes field appears
    in a file schema but not the table schema, this change allows us to
    read (but not materialize) the field. Otherwise we can't read the file
    at all.
    
    This change also adds a "bytes"-type field to one of the files in
    functional_avro_snap.schema_resolution_test.
    
    Change-Id: I25953ee049e174fc4dbff5d68520a6f87e545339
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/3905
    Reviewed-by: Skye Wanderman-Milne <skye@cloudera.com>
    Tested-by: Skye Wanderman-Milne <skye@cloudera.com>

--- be/src/exec/hdfs-avro-scanner.cc
+++ be/src/exec/hdfs-avro-scanner.cc
@@ -421,8 +421,7 @@ HdfsAvroScanner::SchemaElement HdfsAvroScanner::ConvertSchema(
         // not a complex type nor a [<primitive type>, "null"] union itself), we treat
         // this node as the same type as child except with null_union_position set
         // appropriately.
-        if (is_avro_primitive(child.schema.get()) &&
-            child.null_union_position == -1) {
+        if (is_avro_primitive(child.schema.get()) && child.null_union_position == -1) {
           element = child;
           element.null_union_position = null_position;
         }
@@ -632,6 +631,7 @@ void HdfsAvroScanner::MaterializeTuple(MemPool* pool, uint8_t** data, Tuple* tup
         ReadAvroDouble(slot_type, data, write_slot, slot, pool);
         break;
       case AVRO_STRING:
+      case AVRO_BYTES:
         ReadAvroString(slot_type, data, write_slot, slot, pool);
         break;
       case AVRO_DECIMAL: {
@@ -820,6 +820,8 @@ Function* HdfsAvroScanner::CodegenMaterializeTuple(HdfsScanNode* node,
         break;
       default:
         // Unsupported type, can't codegen
+        VLOG(1) << "Failed to codegen MaterializeTuple() due to unsupported type: "
+                << element.schema->type;
         fn->eraseFromParent();
         return NULL;
     }
@@ -874,5 +876,7 @@ Function* HdfsAvroScanner::CodegenDecodeAvroData(LlvmCodeGen* codegen,
     DCHECK_EQ(replaced, 1);
   }
 
-  return codegen->FinalizeFunction(decode_avro_data_fn);
+  decode_avro_data_fn = codegen->FinalizeFunction(decode_avro_data_fn);
+  DCHECK(decode_avro_data_fn != NULL);
+  return decode_avro_data_fn;
 }
