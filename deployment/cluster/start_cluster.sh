6b8713fffea272ebaa868374b935133f8372104a
Robert Navado <sbornava@gmail.com>
2015-01-28 00:12:37 +0200

    Append all output to log file in more readable format
    Additionally, add terminate procedure logging

--- deployment/cluster/start_cluster.sh
+++ deployment/cluster/start_cluster.sh
@@ -21,43 +21,39 @@ fi
 #TODO: Fixup logging
 BATCH_ID=$(uuidgen)
 . resize.config
-LOG=${LOG_DIR}/${BATCH_ID}.log
-LOG_APPEND='tee -a $LOG'
 take_lock
-echo trying to start cluster $BATCH_ID|$LOG_APPEND
+echo $($LOG_PREFIX) Trying to start cluster $BATCH_ID|$LOG_APPEND
 store_cluster_id $BATCH_ID
-echo creating security group if required|$LOG_APPEND
+echo $($LOG_PREFIX) Creating security group if required|$LOG_APPEND
 . create_impala_security_group.sh
-SECURITY_GROUP_IDS=$(get_or_create_security_group ${SECURITY_GROUP})
-echo checking if requested AMI: $IMAGE_ID available|$LOG_APPEND
-IMAGE_AVAILABLE=$($AWS_CMD describe-images --image-ids $IMAGE_ID|grep -o $IMAGE_ID)
+SECURITY_GROUP_IDS=$(get_or_create_security_group ${SECURITY_GROUP}|$LOG_APPEND)
+echo $($LOG_PREFIX) Checking if requested AMI: $IMAGE_ID available|$LOG_APPEND
+IMAGE_AVAILABLE=$($AWS_CMD describe-images --image-ids $IMAGE_ID|$LOG_APPEND|grep -o $IMAGE_ID)
 if [ -z "$IMAGE_AVAILABLE" ];
 then
 	echo Image: $IMAGE_ID is not available
 	echo please check configuration.
 	exit 1
 fi
-echo requesting to start $COUNT instances of $INSTANCE_TYPE size with AMI: $IMAGE_ID
+echo $($LOG_PREFIX) Requesting to start $COUNT instances of $INSTANCE_TYPE size with AMI: $IMAGE_ID|$LOG_APPEND
 
-$AWS_CMD run-instances --image-id $IMAGE_ID --count $COUNT --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_IDS --placement AvailabilityZone=$AVAILABILITY_ZONE --key-name $KEY_NAME --client-token $BATCH_ID --user-data "\'$USER_DATA\'" >> $LOG
+$AWS_CMD run-instances --image-id $IMAGE_ID --count $COUNT --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_IDS --placement AvailabilityZone=$AVAILABILITY_ZONE --key-name $KEY_NAME --client-token $BATCH_ID --user-data "\'$USER_DATA\'" |$LOG_APPEND
 
-echo run-instances request sent, waiting for all instances to run
+echo $($LOG_PREFIX) Run-instances request sent, waiting for all instances to run|$LOG_APPEND
+$AWS_CMD wait instance-running --filters Name=client-token,Values=$BATCH_ID|$LOG_APPEND
 
-$AWS_CMD wait instance-running --filters Name=client-token,Values=$BATCH_ID
+echo $($LOG_PREFIX) All instances running querying instance details |$LOG_APPEND
+$AWS_CMD describe-instances --filters Name=client-token,Values=$BATCH_ID|$LOG_APPEND >$TEMP_FILE
+echo $($LOG_PREFIX) Getting DNS names|$LOG_APPEND
+DNS_NAMES=$(grep $BATCH_ID <${TMP_FILE}|cut -f 14|tee ${CLUSTER_HOSTS}|$LOG_APPEND)
 
-echo all instances running querying instance details 
-TMP_FILE=/tmp/${BATCH_ID}.info
-$AWS_CMD describe-instances --filters Name=client-token,Values=$BATCH_ID >$TMP_FILE
-echo getting DNS names
-DNS_NAMES=$(grep $BATCH_ID <${TMP_FILE}|cut -f 14|tee ${CLUSTER_HOSTS})
-
-echo Adding ssh public-keys records to $SSH_KNOWN_HOSTS_FILE
+echo $($LOG_PREFIX) Adding ssh public-keys records to $SSH_KNOWN_HOSTS_FILE|$LOG_APPEND
 echo -n " " >$SSH_KNOWN_HOSTS_FILE
 #TODO: Add timeout
 #TODO: There is still a possible bug when all hosts does not awaik togather,
 #so when each host exposes more than one key, key count will be greater than required hosts but not enough to connect them all
 while [ $(cat $SSH_KNOWN_HOSTS_FILE|wc -l) -lt $(echo $DNS_NAMES|wc -w) ]; do
-	echo trying to perform keyscan
+	echo $($LOG_PREFIX) Trying to perform keyscan
 	#clear known hosts before attempt
 	echo -n  " " >$SSH_KNOWN_HOSTS_FILE
 	for host in $DNS_NAMES; do
@@ -65,23 +61,23 @@ while [ $(cat $SSH_KNOWN_HOSTS_FILE|wc -l) -lt $(echo $DNS_NAMES|wc -w) ]; do
 	done
 	wait
 done
-echo Added $(cat $SSH_KNOWN_HOSTS_FILE|wc -l) keys for cluster
+echo $($LOG_PREFIX) Keyscan complete. Added $(cat $SSH_KNOWN_HOSTS_FILE|wc -l) keys for cluster|$LOG_APPEND
 
-echo getting instance IDs
+echo $($LOG_PREFIX) Getting instance IDs|$LOG_APPEND
 grep $BATCH_ID <${TMP_FILE}|cut -f8|tee ${CLUSTER_VAR_DIR}/instances
 
 for host in $DNS_NAMES; do
-	echo connecting to $host in background
+	echo $($LOG_PREFIX) Connecting to $host in background|$LOG_APPEND
 	#TODO: You cannot simply run sudo as non-interractive user. Need to discover a way to run it
 	#ssh $SSH_PARAMS ec2-user@$host 'echo command running at `hostname` as user' & 
 	#TODO: generate startup script here, push to target and run in background
+	#TODO: Log the output in some way
 	ssh $SSH_PARAMS ec2-user@$host '"sudo /home/ec2-user/attachToCluster.sh  $ACCESS_KEY $SECRET_KEY $MASTER_NODE $S3_BUCKET" &&  "sudo /home/ec2-user/restart_slave.sh" ' &
-
 done
-echo waiting for all configuration commands to complete
+echo $($LOG_PREFIX) Waiting for all configuration commands to complete|$LOG_APPEND
 wait
 
 rm -f $TMP_FILE
-echo All cluster nodes got configuration command. See master node $MASTER_NODE for details
+echo $($LOG_PREFIX) All cluster nodes got configuration command. See master node $MASTER_NODE for details|$LOG_APPEND
 release_lock
-echo cluster $BATCH_ID is up and running.
+echo $($LOG_PREFIX) Cluster $BATCH_ID is up and running.|$LOG_APPEND
--- deployment/cluster/terminate_cluster.sh
+++ deployment/cluster/terminate_cluster.sh
@@ -8,12 +8,13 @@ then
 	echo "   $0 <cluster id>"
 	exit 1
 fi
+echo $($LOG_PREFIX) Terminating cluster $BATCH_ID|$LOG_APPEND
 take_lock
 function cleanup_cluster_residue(){
 	rm -f $BATCH_ID
 	rm -f $KNOWN_CLUSTERS_DIR/$BATCH_ID
 	rm -rf $CLUSTER_VAR_DIR
-	rm -f $TMP_FILE
+	rm -f $TEMP_FILE
 }
 
 if [ -z "$AWS_CMD" ];
@@ -21,26 +22,27 @@ then
 	AWS_CMD="aws ec2"
 fi
 set -x
-$AWS_CMD describe-instances $DRY_RUN --filters Name=client-token,Values=$BATCH_ID|tee $TEMP_FILE
+echo $($LOG_PREFIX) Query instances for cluster $BATCH_ID|$LOG_APPEND
+$AWS_CMD describe-instances $DRY_RUN --filters Name=client-token,Values=$BATCH_ID|$LOG_APPEND|tee $TEMP_FILE
 
 INSTANCE_IDS=$(grep $BATCH_ID <$TEMP_FILE|cut -f8)
-echo instance list: $INSTANCE_IDS
+echo instance list: $INSTANCE_IDS|$LOG_APPEND
 if [ -z "$INSTANCE_IDS" ];
 then
-	echo Cluster $BATCH_ID has no instances
-	echo Either wrong ID provided, all instances were terminated by other provess or there is residue from other cluster-related scripts
-	cleanup_cluster_residue
+	echo $($LOG_PREFIX) Cluster $BATCH_ID has no instances|$LOG_APPEND
+	echo $($LOG_PREFIX) Either wrong ID provided, all instances were terminated by other provess or there is residue from other cluster-related scripts|$LOG_APPEND
+	cleanup_cluster_residue|$LOG_APPEND
 	release_lock
 	exit 1
 fi
 for instance in $INSTANCE_IDS; do
-	echo found instance: $instance
+	echo $($LOG_PREFIX) found instance: $instance|$LOG_APPEND
 done
-echo Terminating instances: $INSTANCE_IDS
-$AWS_CMD terminate-instances --instance-ids $INSTANCE_IDS
-echo terminate command sent, waiting termination
-$AWS_CMD wait instance-terminated --instance-ids $INSTANCE_IDS
-echo instances terminated, cleanup local resources
-cleanup_cluster_residue
+echo $($LOG_PREFIX) Terminating instances: $INSTANCE_IDS|$LOG_APPEND
+$AWS_CMD terminate-instances --instance-ids $INSTANCE_IDS|$LOG_APPEND
+echo $($LOG_PREFIX) terminate command sent, waiting termination|$LOG_APPEND
+$AWS_CMD wait instance-terminated --instance-ids $INSTANCE_IDS|$LOG_APPEND
+echo $($LOG_PREFIX) instances terminated, cleanup local resources|$LOG_APPEND
+cleanup_cluster_residue|$LOG_APPEND
 release_lock
-echo Done
+echo $($LOG_PREFIX) Done|$LOG_APPEND
