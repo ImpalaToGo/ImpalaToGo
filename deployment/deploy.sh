c98863a30e1492de7643b2917ee094f3b592443e
Robert Navado <sbornava@gmail.com>
2015-04-10 19:12:43 +0300

    Modify deploy script to use packed artifacts

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -15,83 +15,25 @@ TARGET_HOST=$1
 USER=$2
 #authorization 
 KEY=$3
+if [ -d $IMPALA_HOME ];
+then
+BASE_DIR=$IMPALA_HOME
+else
 BASE_DIR=/root/ImpalaToGo
+fi
 
 shift 3
 SSH_PARAMS="-i $KEY"
 REMOTE_TARGET=$USER@$TARGET_HOST
-REMOTE_BASE_DIR=/home/$USER/build
-#tachyon deployment
-TACHYON_DIR=tachyon-0.7.0-SNAPSHOT
-ssh  -i $KEY $REMOTE_TARGET "mkdir -p $REMOTE_BASE_DIR/tachyon/"
-ssh  -i $KEY $REMOTE_TARGET "mkdir -p $REMOTE_BASE_DIR/tachyon/core/target/"
-ssh  -i $KEY $REMOTE_TARGET "mkdir -p $REMOTE_BASE_DIR/tachyon/core/src/main/webapp/"
-ssh  -i $KEY $REMOTE_TARGET "mkdir -p $REMOTE_BASE_DIR/tachyon/client/target/"
-ssh  -i $KEY $REMOTE_TARGET "mkdir -p $REMOTE_BASE_DIR/tachyon/assembly/target/"
-ssh  -i $KEY $REMOTE_TARGET "mkdir -p $REMOTE_BASE_DIR/tachyon/conf/"
-ssh  -i $KEY $REMOTE_TARGET "mkdir -p $REMOTE_BASE_DIR/tachyon/bin/"
-ssh  -i $KEY $REMOTE_TARGET "mkdir -p $REMOTE_BASE_DIR/tachyon/libexec/"
-ssh  -i $KEY $REMOTE_TARGET "mkdir -p $REMOTE_BASE_DIR/llvm-ir/"
-ssh  -i $KEY $REMOTE_TARGET "mkdir -p $REMOTE_BASE_DIR/be/"
-ssh  -i $KEY $REMOTE_TARGET "mkdir -p $REMOTE_BASE_DIR/thirdparty/thrift/build/"
-ssh  -i $KEY $REMOTE_TARGET "mkdir -p $REMOTE_BASE_DIR/bin/"
-ssh  -i $KEY $REMOTE_TARGET "mkdir -p $REMOTE_BASE_DIR/shell/"
-ssh  -i $KEY $REMOTE_TARGET "mkdir -p $REMOTE_BASE_DIR/lib/"
- 
-scp -rp -i $KEY $BASE_DIR/thirdparty/$TACHYON_DIR/core/target/*.jar $REMOTE_TARGET:$REMOTE_BASE_DIR/tachyon/core/target/
-scp -rp -i $KEY $BASE_DIR/thirdparty/$TACHYON_DIR/core/src/main/webapp/* $REMOTE_TARGET:$REMOTE_BASE_DIR/tachyon/core/src/main/webapp/
-scp -rp -i $KEY $BASE_DIR/thirdparty/$TACHYON_DIR/client/target/*.jar $REMOTE_TARGET:$REMOTE_BASE_DIR/tachyon/client/target/
-scp -rp -i $KEY $BASE_DIR/thirdparty/$TACHYON_DIR/assembly/target/*.jar $REMOTE_TARGET:$REMOTE_BASE_DIR/tachyon/assembly/target/
-
-scp -rp -i $KEY $BASE_DIR/thirdparty/$TACHYON_DIR/conf/* $REMOTE_TARGET:$REMOTE_BASE_DIR/tachyon/conf/
-scp -rp -i $KEY $BASE_DIR/thirdparty/$TACHYON_DIR/bin/* $REMOTE_TARGET:$REMOTE_BASE_DIR/tachyon/bin/
-scp -rp -i $KEY $BASE_DIR/thirdparty/$TACHYON_DIR/libexec/* $REMOTE_TARGET:$REMOTE_BASE_DIR/tachyon/libexec/
-
-#impala deployment
-scp -rp -i $KEY $BASE_DIR/llvm-ir/* $REMOTE_TARGET:$REMOTE_BASE_DIR/llvm-ir/
-
-scp -rp -i $KEY $BASE_DIR/be/build/debug/catalog/catalogd $REMOTE_TARGET:$REMOTE_BASE_DIR/be/
-scp -rp -i $KEY $BASE_DIR/be/build/debug/statestore/statestored   $REMOTE_TARGET:$REMOTE_BASE_DIR/be/
-
-#thirdparty deployment
-scp -rp -i $KEY $BASE_DIR/thirdparty/thrift-0.9.0/build/*  $REMOTE_TARGET:$REMOTE_BASE_DIR/thirdparty/thrift/build/
-
-scp  -i $KEY $BASE_DIR/bin/set-pythonpath.sh $REMOTE_TARGET:$REMOTE_BASE_DIR/bin/
-
-#shell deployment
-scp -rp  -i $KEY $BASE_DIR/shell/* $REMOTE_TARGET:$REMOTE_BASE_DIR/shell/
-scp  -i $KEY $BASE_DIR/bin/impala-shell.sh $REMOTE_TARGET:$REMOTE_BASE_DIR/bin/
-scp -i $KEY $BASE_DIR/deployment/cluster/control/shell.sh $REMOTE_TARGET:$REMOTE_BASE_DIR/bin/
-#backend
-scp  -i $KEY $BASE_DIR/be/build/debug/service/* $REMOTE_TARGET:$REMOTE_BASE_DIR/be
-
-#webap
-ssh  -i $KEY $REMOTE_TARGET "mkdir -p $REMOTE_BASE_DIR/www/"
-scp -rp -i $KEY $BASE_DIR/www/* $REMOTE_TARGET:$REMOTE_BASE_DIR/www
-
-#frontend
-ssh  -i $KEY $REMOTE_TARGET "mkdir -p $REMOTE_BASE_DIR/fe/"
-scp  -i $KEY $BASE_DIR/fe/target/impala-frontend-0.1-SNAPSHOT.jar $REMOTE_TARGET:$REMOTE_BASE_DIR/fe
-scp -rp  -i $KEY  ${IMPALA_HOME}/shell/*  $REMOTE_TARGET:$REMOTE_BASE_DIR/shell/
-
-#jars dependencies deployment
-ssh  -i $KEY $REMOTE_TARGET "mkdir -p $REMOTE_BASE_DIR/fe/dependency/"
-for jar in $(ls ${IMPALA_HOME}/fe/target/dependency/*.jar); do
- scp  -i $KEY $jar $REMOTE_TARGET:$REMOTE_BASE_DIR/fe/dependency/
-done
-
-#boost deployment
-scp  -i $KEY /usr/lib/x86_64-linux-gnu/libboost_thread.so.1.54.0  $REMOTE_TARGET:$REMOTE_BASE_DIR/lib
-scp  -i $KEY /usr/lib/x86_64-linux-gnu/libboost_regex.so.1.54.0  $REMOTE_TARGET:$REMOTE_BASE_DIR/lib
-scp  -i $KEY /usr/lib/x86_64-linux-gnu/libboost_date_time.so.1.54.0  $REMOTE_TARGET:$REMOTE_BASE_DIR/lib
-scp  -i $KEY /usr/lib/x86_64-linux-gnu/libboost_filesystem.so.1.54.0  $REMOTE_TARGET:$REMOTE_BASE_DIR/lib
-scp  -i $KEY /usr/lib/x86_64-linux-gnu/libboost_system.so.1.54.0  $REMOTE_TARGET:$REMOTE_BASE_DIR/lib
-
-#libicu deployment
-scp  -i $KEY /usr/lib/x86_64-linux-gnu/libicuuc.so.52.1  $REMOTE_TARGET:$REMOTE_BASE_DIR/lib
-scp  -i $KEY /usr/lib/x86_64-linux-gnu/libicui18n.so.52.1  $REMOTE_TARGET:$REMOTE_BASE_DIR/lib
-scp  -i $KEY /usr/lib/x86_64-linux-gnu/libicudata.so.52.1  $REMOTE_TARGET:$REMOTE_BASE_DIR/lib
-
-#deployment section
-scp  -i $KEY $BASE_DIR/deployment/install.sh $REMOTE_TARGET:$REMOTE_BASE_DIR/
-
+REMOTE_BASE_DIR=/home/$USER/
+echo -n Copying build results to target...
+scp -v $SSH_PARAMS $BASE_DIR/build.tgz $REMOTE_TARGET:$REMOTE_BASE_DIR
+echo Done
+echo -n Unpacking build results on target...
+#TODO: Cleanup target directory
+ssh $SSH_PARAME $REMOTE_TARGET tar -xzf $REMOTE_BASE_DIR/build.tgz
+echo Done
+echo -n Running install script on target...
+#TODO: Remove previous version before installing
+ssh $SSH_PARAME $REMOTE_TARGET $REMOTE_BASE_DIR/build/deployment/install.sh
+echo Done
